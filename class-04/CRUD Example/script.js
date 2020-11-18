f9371bb143dd35093aa0e95f4b7f857119cffd64
Haris Zenovic <Haris.Zenovic@seavus.com>
2020-11-18 19:58:51 +0100

    added edit and delete functionalities

--- class-04/CRUD Example/script.js
+++ class-04/CRUD Example/script.js
@@ -1,3 +1,25 @@
+function LocalFormData(form) {
+  this.firstName = form[0].value ? form[0].value : form[0].textContent;
+  this.lastName = form[1].value ? form[1].value : form[1].textContent;
+  this.age = form[2].value ? form[2].value : form[2].textContent;
+  this.department = form[3].value ? form[3].value : form[3].textContent;
+
+  this.getParamInputField = function(varName) {
+    return `<input value="${this[varName]}"/>`;
+  } 
+  this.getSelectInputForDepartments = function() {
+    return `
+      <select id="department">
+        <option value="hr">HR</option>
+        <option value="dev">Dev</option>
+        <option value="finance">Finance</option>
+        <option value="management">Management</option>
+      </select>
+    `;
+  }
+}
+
+
 var getDataFromForm = function (event) {
   // ne efikasan nacin
   // var firstName = document.getElementById("first-name").nodeValue;
@@ -6,23 +28,96 @@ var getDataFromForm = function (event) {
   var form = event.currentTarget;
 
   console.log(form);
-  var data = {
-    firstName: form[0].value,
-    lastName: form[1].value,
-    age: form[2].value,
-    department: form[3].value,
-  };
+  var data = new LocalFormData(form);
+  console.log(data);
+  // var data = {
+  //   firstName: form[0].value,
+  //   lastName: form[1].value,
+  //   age: form[2].value,
+  //   department: form[3].value,
+  // };
   form.reset();
   return data;
 };
+
+var editActionFunc = function (event) {
+  var trow = event.currentTarget.parentElement.parentElement;
+  var data = new LocalFormData(trow.children);
+  // trow.
+  console.log(data);
+
+  // trow.children[0].innerHTML = `<input value="${data.firstName}"/>`;
+  trow.children[0].innerHTML = data.getParamInputField('firstName');
+  trow.children[1].innerHTML = data.getParamInputField('lastName');
+  trow.children[2].innerHTML = data.getParamInputField('age');
+  trow.children[3].innerHTML = data.getSelectInputForDepartments();
+  trow.children[4].innerHTML = "";
+
+  var saveAction = document.createElement("button");
+  saveAction.textContent = `Save`;
+
+  saveAction.addEventListener("click", function (saveEvent) {
+    if (
+      trow.children[0].firstChild.value.match(letters) &&
+      trow.children[1].firstChild.value.match(letters) &&
+      trow.children[2].firstChild.value.match(num)
+    ) {
+      var row = saveEvent.currentTarget.parentElement.parentElement;
+      trow.children[0].innerHTML = trow.children[0].firstChild.value;
+      trow.children[1].innerHTML = trow.children[1].firstChild.value;
+      trow.children[2].innerHTML = trow.children[2].firstChild.value;
+      trow.children[3].innerHTML = trow.children[3].firstChild.nextSibling.value;
+      
+      trow.children[4].appendChild(getEditButton());
+      trow.children[4].appendChild(getDeleteButton());
+
+    } else {
+      alert("Podaci nisu validni");
+    }
+  });
+
+  var discartAction = document.createElement("button");
+  discartAction.textContent = `Discart`;
+
+  trow.children[4].appendChild(saveAction);
+  trow.children[4].appendChild(discartAction);
+}
+
+var deleteActionFunc = function (event) {
+  var result = window.confirm("Are you sure you want to delete this row ?");
+  if (result) {
+    var trow = event.currentTarget.parentElement.parentElement;
+    trow.remove();
+  }
+}
+
+var getEditButton = function() {
+  var editAction = document.createElement("button");
+  editAction.textContent = `Edit`;
+  
+  // same as addEventListener
+  editAction.addEventListener("click", editActionFunc);
+
+  return editAction;
+}
+
+var getDeleteButton = function() {
+  var deleteAction = document.createElement("button");
+  deleteAction.textContent = `Delete`;
+
+  deleteAction.addEventListener('click', deleteActionFunc);
+
+  return deleteAction;
+}
+
+var letters = /^[A-Za-z]+$/;
+var num = /^[1-9][0-9]+$/;
 
 var addDataIntoTable = function (data) {
   var table = document.getElementById("list-of-employees");
   var row = document.createElement("tr");
 
   //validation
-  var letters = /^[A-Za-z]+$/;
-  var num = /^[0-9]+$/;
   if (
     data.firstName.match(letters) &&
     data.lastName.match(letters) &&
@@ -49,56 +144,10 @@ var addDataIntoTable = function (data) {
 
     // });
     // same as addEventListener
-    editAction.addEventListener("click", function (event) {
-      var trow = event.currentTarget.parentElement.parentElement;
-      var data = {
-        firstName: trow.children[0].textContent,
-        lastName: trow.children[1].textContent,
-        age: trow.children[2].textContent,
-        department: trow.children[3].textContent,
-      };
-      // trow.
-      console.log(data);
+    editAction.addEventListener("click", editActionFunc);
 
-      trow.children[0].innerHTML = `<input value="${data.firstName}"/>`;
-      trow.children[1].innerHTML = `<input value="${data.lastName}"/>`;
-      trow.children[2].innerHTML = `<input value="${data.age}"/>`;
-      trow.children[3].innerHTML = `<input value="${data.department}"/>`;
-      trow.children[4].innerHTML = "";
-
-      var saveAction = document.createElement("button");
-      saveAction.textContent = `Save`;
-
-      saveAction.addEventListener("click", function (saveEvent) {
-        if (
-          trow.children[0].firstChild.value.match(letters) &&
-          trow.children[1].firstChild.value.match(letters) &&
-          trow.children[2].firstChild.value.match(num)
-        ) {
-          var row = saveEvent.currentTarget.parentElement.parentElement;
-          trow.children[0].innerHTML = trow.children[0].firstChild.value;
-          trow.children[1].innerHTML = trow.children[1].firstChild.value;
-          trow.children[2].innerHTML = trow.children[2].firstChild.value;
-          trow.children[3].innerHTML = trow.children[3].firstChild.value;
-          trow.children[4].innerHTML = `<button>Edit</button><button>Delete</button>`;
-          // debugger;
-        } else {
-          alert("Podaci nisu validni");
-        }
-      });
-
-      var discartAction = document.createElement("button");
-      discartAction.textContent = `Discart`;
-
-      trow.children[4].appendChild(saveAction);
-      trow.children[4].appendChild(discartAction);
-      // debugger;
-    });
-
-    var deleteAction = document.createElement("button");
-    deleteAction.textContent = `Delete`;
-    actionCell.appendChild(editAction);
-    actionCell.appendChild(deleteAction);
+    actionCell.appendChild(getEditButton());
+    actionCell.appendChild(getDeleteButton());
 
     row.appendChild(actionCell);
 
@@ -110,7 +159,56 @@ var form = document.getElementById("form-input");
 console.log(form);
 
 form.addEventListener("submit", function (event) {
+  event.preventDefault();
   var data = getDataFromForm(event);
   addDataIntoTable(data);
-  event.preventDefault();
 });
+
+var sortTableByColumn = function(event) {
+  var ths = document.querySelectorAll('th');
+  var thsArray = Array.from(ths);
+  for(var preTh of thsArray) {
+    preTh.style.backgroundColor = 'white';
+  }
+  var th = event.currentTarget;
+  th.style.backgroundColor = 'gray';
+
+  var sortByProp = th.getAttribute('data-prop');
+
+  var tableRows = document.querySelectorAll("table > tr");
+  var tableRowsArray = Array.from(tableRows);
+
+  var datObj = [];
+
+  for(var tr of tableRowsArray) {
+    
+    datObj.push(new LocalFormData(tr.children));
+
+  }
+  datObj.sort(function(a, b) {
+    if(typeof a[sortByProp] == 'string') {
+      return a[sortByProp].split('')[0] < b[sortByProp].split('')[0]
+    }
+    return a[sortByProp] < b[sortByProp];
+  });
+
+  for(var rowOfTable of tableRows) {
+    rowOfTable.remove();
+  }
+  for(var data of datObj) {
+    addDataIntoTable(data);
+  }
+
+}
+// 
+var setEventListenersOnTh = function() {
+  var ths = document.getElementsByTagName('th');
+  var thsArray = Array.from(ths);
+  for (var i = 0; i < thsArray.length - 1; i++) {
+    thsArray[i].addEventListener("click", sortTableByColumn)
+  }
+  console.log(ths);
+  debugger;
+}
+setEventListenersOnTh();
+
